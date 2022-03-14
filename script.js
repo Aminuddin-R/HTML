b608d40679a3e22c85adb24fe615a54511005186
Aminuddin-R <72755037+Aminuddin-R@users.noreply.github.com>
2022-03-14 07:49:29 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var time = new Date();
+document.getElementById("datetime").innerHTML = time.toLocaleString();
+
+const x = document.querySelector('.burger input')
+const header = document.querySelector('header ul')
+
+x.addEventListener('click',function(){
+    header.classList.toggle('move');
+})
